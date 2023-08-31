ae3079494f6e3835925f81c70121a66077d50d89
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-08-31 09:17:09 -0400

    finished refactoring code from html to react and deleted any unused files.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import linkedinSVG from './assets/linkedin-logo.svg';
 import githubSVG from './assets/GITHUB-LOGO.svg';
+import lsampSVG from './assets/lsamp-webpage.svg'
 
 function SectionTitle({sectionName}) {
   return (
@@ -15,14 +16,14 @@ function SectionTitle({sectionName}) {
 function Experience({jobTitle, company, start, end, description, software}) {
 
   return (
-    <div>
+    <div><br /><br /><br />
                 <h3>
-                {jobTitle ? jobTitle : "Default"} - {company ? company : "Default"}
-                {start ? start : "Default"} - {end ? end : "Default"}
+                {jobTitle ? jobTitle : "Default"} - {company ? company : "Default"}<br /><br />
+                {start ? start : "Default"} - {end ? end : "Default"}<br /><br />
                 </h3>
 
                 <p>
-                {description ? description : "Default"}
+                {description ? description : "Default"}<br /><br />
                     
                     <b>{software ? software : "Default"}</b>
                 </p>
@@ -30,17 +31,51 @@ function Experience({jobTitle, company, start, end, description, software}) {
   );
 }
 
+function Project ({projName, workProof, img, alt, software}) {
+
+  return (
+    <div>
+      <div><br /><br /><br />
+        {projName}
+      </div>
+
+      <div><br /><br />
+      <a href={workProof} target="_blank"><img src={img} 
+      height="600px" width="700px" alt={alt}/></a>
+      </div><br /><br />
+      <b>{software}</b>
+    </div>
+  );
+}
+
 function App() {
+  const descJ1 = "Implemented Software Development Cycle for website front and backend,"+
+  " maintained web domain with respect to Rutgers web protocols and tracked changes with version control software.";
+  const softwareJ1 = "CSS - HTML - JavaScript - PHP - PHPMailer - Git - GitHub - Figma - WAMP";
+
+  const descJ2 ="Built entity relationship diagrams and implemented in MySQL. Formulate queries to store user input to database and "+
+  "created triggers for auto generation of records in other tables. Normalized tables for first normal form.";
+  const softwareJ2 = "MySQL - Lucid Chart";
+
+  const descJ3 = "Developed code to conduct experiments in a Head-Mounted Display using Psychtoolbox library and others in MATLAB."+
+  "Installed appropriate drivers for HMD to display stimuli. Used Vizard to create virtual environments in HMD. Use summary statistics,"+
+  "distribution plots and joy plots in R for easy assesment of experiment data.";
+  const softwareJ3 = "Python - Linux - MATLAB - R Studio";
+
+  const descJ4 ="Created a SQL server database through Azure, and a document management system using C# asp.NET framework to manage"+
+  " and sort faculty records.";
+  const softwareJ4 ="ASP.NET - SQL Server - C# - Microsoft Azure";  
   return (
     <body>
       <nav class=" bg-[#D9D9D9] block items-center p-1">
         <ul class="m-0 p-0 list-none items-center flex flex-row-reverse">
           <li>
-            <a class="hover:scale-75 transition delay-200 px-6 block" href="https://www.linkedin.com/in/christino-barbosa-8a4587197/" 
-                target="_blank"><img src={linkedinSVG} height='80' width='80' alt="LinkedIn"/></a>
+            <a class="hover:scale-75 transition delay-200 px-4 block" href="https://www.linkedin.com/in/christino-barbosa-8a4587197/" 
+                target="_blank"><img src={linkedinSVG} height='70' width='70' alt="LinkedIn"/></a>
           </li>
           <li>
-          <a class="hover:scale-75 transition delay-200 px-6 block" href="https://github.com/christinolb" target="_blank"><img src={githubSVG} width="100" height="100" alt="GitHub"/></a>
+          <a class="hover:scale-75 transition delay-200 px-2 block" href="https://github.com/christinolb" target="_blank">
+            <img src={githubSVG} width="100" height="100" alt="GitHub"/></a>
           </li>
           <li>
             <a class="hover:text-white transition delay-200 px-8 block" href="#projects">Projects</a>
@@ -74,9 +109,27 @@ function App() {
 
       <SectionTitle sectionName="Experience" />
       <>
-      <Experience />
+        <Experience jobTitle="Web Developer" company="Rutgers Newark LSAMP" start="January 2023" end="August 2023" 
+        description={descJ1} software={softwareJ1}/>
+
+        <Experience jobTitle="Database Developer" company="Rutgers IT" start="February 2023" end="May 2023" 
+        description={descJ2} software={softwareJ2}/> 
+
+        <Experience jobTitle="Research Assistant" company="Rutgers Data Science" start="January 2022" end="December 2022" 
+        description={descJ3} software={softwareJ3}/>
+
+        <Experience jobTitle="Project Leader" company="Union County College" start="January 2021" end="August 2021" 
+        description={descJ4} software={softwareJ4}/>
+
       </>
-      
+
+      <SectionTitle sectionName="Projects" />
+      <>
+        <Project projName="Rutgers University GS-LSAMP Website" workProof="https://github.com/rutgers-newark-gslsamp/run-gslsamp-site"
+         img={lsampSVG} alt="Mockup Contact Us Page" software="HTML - CSS - PHP - PHPMailer - GitHub - Git - Figma"/>
+
+        <Project projName="Course Of Study Database" img="" alt="ERD" software="SQL Server - Lucid Chart"/>
+      </>
 
 
     
