d8518d6005158826f52d70f4e8ed5c7fb25d0a79
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-11 16:40:26 -0400

    created journal page, needs connection to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /* src/App.jsx */
 import React from "react";
+import Nav from './Nav';
+
 import "./App.css";
-import linkedinSVG from './assets/linkedin-logo.svg';
-import githubSVG from './assets/GITHUB-LOGO.svg';
 import abstract from './assets/papers-presentations/abstract_BR.pdf';
 import posterBr from './assets/papers-presentations//ChristinoResearchPoster.pdf';
 import frfdmsPaper from'./assets/papers-presentations/FRFDMS-paper.pdf';
@@ -10,29 +10,8 @@ import frfdmsProcess from './assets/papers-presentations/FRFDMSProcessGeneral.pd
 import robustPaper from './assets/papers-presentations/Robustness-of-a-Two-Stage-Procedure_paper.pdf';
 import robustPoster from './assets/papers-presentations/Robustness-of-a-Two-Stage-Procedure.pdf';
 import sasnReport from './assets/papers-presentations/sasn_course_of_study_report.pdf';
+import { Router } from "react-router-dom";
 
-function Nav (){
-  return(
-    <nav id="index" class="border-b-[2px] p-1">
-        <ul class="m-0 p-0 list-none items-center flex flex-row-reverse text-2xl">
-          <li>
-            <a class="hover:scale-75 transition delay-150 px-4 block" href="https://www.linkedin.com/in/christino-barbosa-8a4587197/" 
-                target="_blank"><img src={linkedinSVG} height='70' width='70' alt="LinkedIn"/></a>
-          </li>
-          <li>
-          <a class="hover:scale-75 transition delay-150 px-2 block" href="https://github.com/christinolb" target="_blank">
-            <img src={githubSVG} width="100" height="100" alt="GitHub"/></a>
-          </li>
-          <li>
-            <a class="hover:scale-75 transition delay-150 px-8 block" href="#experience">Experience & Work</a>
-          </li>
-          <li>
-            <a class="hover:scale-75 transition delay-150 px-8 block" href="#index">About</a>
-          </li>
-        </ul>
-      </nav>
-  );
-}
 
 function Experience({jobTitle, company, start, end, description, software, workResults}) {
 
